1101f45bc85a7f57232db74911385ab08c549d5e
Chaust0376 <70907066+Chaust0376@users.noreply.github.com>
2020-10-16 09:02:14 -0500

    Flee Implemented

---
+++ Project 4/Assets/Scripts/FleePointer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class FleePointer : MonoBehaviour
+{
+    public Transform target;
+    public float moveSpeed = 6.0f;
+    private float maxDistance = 15.0f;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Call to the fucntion every frame
+        FleeTarget();
+    }
+
+    void FleeTarget()
+    {
+        //Subtracting two vectors by each will result in the desired direction
+        Vector3 dir = target.position - transform.position;
+        //Simple check to see if we continue seeking the target or if we are already at our desired distance from target
+        if (dir.magnitude < maxDistance)
+        {
+            Vector3 moveVector = dir.normalized * moveSpeed * Time.deltaTime;
+            //If the case check true, we continue moving towards our target
+            transform.position -= moveVector;
+        }
+    }
+}
