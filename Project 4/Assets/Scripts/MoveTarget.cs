2a5303bae3cc4362afb10e3aac50e0e25e9dd28e
Chaust0376 <70907066+Chaust0376@users.noreply.github.com>
2020-10-16 08:47:55 -0500

    Seek Implemented

---
+++ Project 4/Assets/Scripts/MoveTarget.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class MoveTarget : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            UpdateTargetPosition();
+        }
+    }
+    void UpdateTargetPosition()
+    {
+        // Don’t forget to add a plane on the ground in the Unity Scene
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hit, 100))
+        {
+            transform.position = hit.point;
+        }
+    }
+}
---
+++ Project 4/Assets/Scripts/Seek.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seek : MonoBehaviour
+{
+    public Transform target;
+    public float moveSpeed = 6.0f;
+    private float minDistance = 0.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Call to the fucntion every frame
+        SeekTarget();
+    }
+
+
+    void SeekTarget()
+    {
+        //Subtracting two vectors by each will result in the desired direction
+        Vector3 dir = target.position - transform.position;
+        //Simple check to see if we continue seeking the target or if we are already at our desired distance from target
+        if (dir.magnitude > minDistance)
+        {
+            Vector3 moveVector = dir.normalized * moveSpeed * Time.deltaTime;
+            //If the case check true, we continue moving towards our target
+            transform.position += moveVector;
+        }
+    }
+}
