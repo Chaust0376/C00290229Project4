ec47f92368d1a5e4d9c3cdf9d0897d634c0bc3af
Chaust0376 <70907066+Chaust0376@users.noreply.github.com>
2020-10-16 17:10:29 -0500

    Update Flock.cs

--- Project 4/Assets/Scripts/Flock.cs
+++ Project 4/Assets/Scripts/Flock.cs
@@ -6,7 +6,11 @@ using UnityEngine;
 
 public class Flock : MonoBehaviour
 {
-    public float speed = 0.1f;
+    public float speed = 0.001f;
+    float rotationSpeed = 4.0f;
+    Vector3 averageHeading;
+    Vector3 averagePosition;
+    float neightbourDistance = 2.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +21,20 @@ public class Flock : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Random.Range(0, 5) < 1)
+        {
+            ApplyRules();
+        }
         transform.Translate(0, 0, Time.deltaTime * speed);
+    }
+    void ApplyRules()
+    {
+        GameObject[] gos;
+        gos = globalFlock.allLeaves;
+
+        Vector3 vcenter = Vector3.zero;
+        Vector3 vavoid = Vector3.zero;
+        float gSpeed = 0.1f;
+
     }
 }
